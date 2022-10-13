eb1dfffd9d4939d78bb193e2030c2e65a17e749d
AntonioliBenjamin <benantonioli@outlook.fr>
2022-10-13 12:04:07 +0200

    feat/check-internet-connection

---
+++ check_connection.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#ping -c 3 google.com
+echo -e "GET http://google.com HTTP/1.0\n\n" | nc google.com 80 > /dev/null 2>&1
+
+if [ $? -eq 0 ]; then
+    status=online
+else
+    status=offline
+fi
+
--- hey_mercedes.sh
+++ hey_mercedes.sh
@@ -9,16 +9,37 @@ else	echo "Je n'ai pas saisi votre demande" && exit;
 fi
 
 if [ "$input" == "weather" ]; then
-	source ./weather_api.sh;
-	restart;
+	source ./check_connection.sh
+# La variable status est crée dans le fichier check_connection.sh	
+		if [ "$status" == "online" ]; then
+			source ./weather_api.sh;
+			restart;
+		else
+			source ./weather.sh;
+			restart;
+		fi
 
 elif  [ "$input" == "address" ]; then
-	source ./address_api.sh;
-	restart;
+	source ./check_connection.sh
+		
+		if [ "$status" == "online" ]; then
+			source ./address_api.sh;
+			restart;
+		else
+			source ./address.sh;
+			restart;
+		fi
 
 elif [ "$input" == "quoteday" ]; then
-	source ./quoteday_api.sh;
-	restart;
+	source ./check_connection.sh
+		
+		if [ "$status" == "online" ]; then
+			source ./quoteday_api.sh;
+			restart;
+		else
+			source ./quoteday.sh;
+			restart;
+		fi
 
 elif [ "$input" == "bye_mercedes" ]; then
 	exit;
